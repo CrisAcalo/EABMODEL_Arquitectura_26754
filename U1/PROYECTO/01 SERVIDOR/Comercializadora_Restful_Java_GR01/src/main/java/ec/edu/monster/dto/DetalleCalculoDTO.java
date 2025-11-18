1aa56eb9c528b5607f72532f1514ed79605e05ae
OppaDev <leonardojeffer.145@gmail.com>
2025-11-17 22:29:58 -0500

    metodo calcular total factura corregido en java

---
+++ U1/PROYECTO/01 SERVIDOR/Comercializadora_Restful_Java_GR01/src/main/java/ec/edu/monster/dto/DetalleCalculoDTO.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.monster.dto;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * Detalle de cada item en el cálculo
+ */
+public class DetalleCalculoDTO implements Serializable {
+
+    private Integer productoId;
+    private String nombreProducto;
+    private Integer cantidad;
+    private BigDecimal precioUnitario;
+    private BigDecimal subtotal;
+
+    // Constructores
+    public DetalleCalculoDTO() {
+    }
+
+    public DetalleCalculoDTO(Integer productoId, String nombreProducto, Integer cantidad,
+                             BigDecimal precioUnitario, BigDecimal subtotal) {
+        this.productoId = productoId;
+        this.nombreProducto = nombreProducto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.subtotal = subtotal;
+    }
+
+    // Getters y Setters
+    public Integer getProductoId() {
+        return productoId;
+    }
+
+    public void setProductoId(Integer productoId) {
+        this.productoId = productoId;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public BigDecimal getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(BigDecimal precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public BigDecimal getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(BigDecimal subtotal) {
+        this.subtotal = subtotal;
+    }
+}
